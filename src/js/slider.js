1d9946c7de9f61e51ebd8bd6cb8b5b45a5bf684a
Vito <doktormc25@gmail.com>
2020-06-10 00:22:35 +0300

    Carclass & OrderBy parts

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.page_baner-slider').slick({
-    arrows : false,
+    arrows: false,
     dots: true
   });
+
+  //car class slider
+
+  $('.carclass_slider').slick({
+    infinite: true,
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding: '0',
+  });
+
+//orderby slider
+  $('.orderby_slider').slick({
+    arrows: true,
+    dots: false,
+    fade: true,
+    cssEase: 'linear',
+    
+  });
+  
 });
+
+
+
